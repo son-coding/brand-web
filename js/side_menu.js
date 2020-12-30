e206ca7ac1a9547f1774f4eabd039deb25f99bd9
son-coding <64684390+son-coding@users.noreply.github.com>
2020-12-30 12:25:53 +0900

    Add files via upload

---
+++ js/side_menu.js
@@ -0,0 +1,9 @@
+function menu_open() {
+  document.getElementById("side_menu").style.width = "250px";
+  document.getElementById("overlay").style.width="100%";
+}
+
+function menu_close() {
+  document.getElementById("side_menu").style.width = "0";
+  document.getElementById("overlay").style.width="0";
+}
